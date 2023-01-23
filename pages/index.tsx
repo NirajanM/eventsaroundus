733eb36645f1ee6f38568be4367adf30a9cf9da6
Nirajan Malla <nirajannitro5@gmail.com>
2023-01-23 13:04:55 +0545

    added json data and created homepage with routes link

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,9 +11,38 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <header>
+        <nav>
+          <a href="/">Home</a>
+          <a href="/events">Events</a>
+          <a href="/about-us">About Us</a>
+        </nav>
+      </header>
       <main className={styles.main}>
-        <h1>Next Js is summoned</h1>
+        <a href="/events/london">
+          <img /><h2>Events in London</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut consequatur optio consectetur eaque, repellendus reprehenderit incidunt ad pariatur nisi cumque dolorem aliquam veniam tempore modi laudantium alias totam error impedit? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora, error voluptatum illum omnis eius esse ut pariatur quaerat sit, laudantium quibusdam aliquid? Dolorem culpa exercitationem necessitatibus inventore. Facilis, reiciendis nesciunt!
+          </p>
+        </a>
+        <a href="/events/sanfrancisco">
+          <img /><h2>Events in Sanfrancisco</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos vitae aperiam dicta neque perspiciatis beatae accusantium maiores ut, sunt debitis impedit, ea esse eos non adipisci reprehenderit vel dolor modi?Fugiat sit inventore aspernatur. Sed modi voluptatum possimus provident molestiae, eveniet nam explicabo temporibus quidem id laboriosam cupiditate ad velit recusandae itaque repellat nisi esse ducimus aperiam? Obcaecati, laboriosam minima? Autem nihil architecto exercitationem vero veritatis laboriosam explicabo repellat sapiente sit officia quam in nostrum, magnam voluptas eos, accusantium molestiae blanditiis? Ducimus optio voluptatibus impedit laboriosam, voluptates ut illum nisi!
+          </p>
+        </a>
+        <a href="/events/Barcelona">
+          <img /><h2>Events in Barcelona</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsum eveniet accusantium excepturi pariatur, eligendi accusamus magnam. Iure, itaque? Reprehenderit excepturi nihil distinctio ullam sequi quod tempore temporibus amet quibusdam?Vel ipsum nostrum asperiores laboriosam blanditiis dolorum consequatur magnam recusandae officia tempore dolor odio beatae quisquam quibusdam, quidem saepe ea sapiente. Porro praesentium veniam ratione, sint saepe quod neque nostrum.Aut neque et, voluptatem ea ut cumque pariatur! Libero maxime nam tempora dolorem quam soluta facilis, voluptate a quae esse iusto eligendi earum quas iure recusandae fugiat eius id laudantium.
+          </p>
+        </a>
       </main>
+      <footer>
+        <div>
+          &copy; {new Date().getFullYear()} all rights reserved
+        </div>
+      </footer>
     </>
   )
 }
