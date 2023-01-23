4b7e084dbde7e3432d4bf6bdb6422d76deeafe7c
Nirajan Malla <nirajannitro5@gmail.com>
2023-01-23 17:17:08 +0545

    lorem epsum about us

---
+++ pages/about-us.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function AboutUs() {
+    return (
+        <div>
+            <h1>About Us:</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure saepe tempora molestiae hic, quae impedit accusamus, ipsa quos consequuntur odio, dolores repudiandae minima. Ut explicabo nostrum unde iusto cum ullam.
+                Eum cum nihil officia enim corrupti quia veniam laboriosam dicta vel vitae cupiditate temporibus non, perspiciatis, consectetur, eius libero facilis ex excepturi eligendi laudantium. Iste molestias error eligendi veritatis dicta!
+                Minus laboriosam excepturi facilis molestias obcaecati saepe exercitationem, incidunt, aut molestiae iure commodi iste nemo vitae. Optio, ducimus rerum temporibus enim, ipsum iure voluptatum, aliquam pariatur laudantium excepturi dolor reprehenderit?
+                Aspernatur id, eos magnam voluptates mollitia accusamus commodi deleniti animi? Minus totam, voluptatibus, architecto sapiente atque aut hic voluptatum velit vitae earum obcaecati assumenda. Aspernatur dicta facilis laborum totam iste.
+                Sunt unde voluptas sapiente expedita amet provident natus error facere reprehenderit, aut accusamus totam tempora libero iste dolorem quas voluptates saepe quod perferendis ea! Doloremque dicta aut quidem sed sequi.
+                Ut rem velit, reprehenderit tempore voluptates, tempora itaque perspiciatis odit dolores, eum in necessitatibus maiores molestias et eligendi quam non? Pariatur vel itaque incidunt iusto nesciunt natus expedita esse corrupti.
+                Debitis rem molestiae incidunt officiis deleniti! Nobis numquam repudiandae enim accusamus, ipsam tempora voluptatem aspernatur asperiores nam nesciunt suscipit pariatur qui maiores dolorem libero laboriosam omnis magni laborum ea cupiditate.
+                Nihil sint culpa doloremque incidunt provident odio veniam! Deleniti tempora non quasi ipsa consequatur provident in numquam labore dolore eos! Vel provident in, quibusdam veritatis et voluptatem impedit sint rem.</p>
+        </div>
+    )
+}
